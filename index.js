3876c905ac310cf290176bdeae67503e6fee737f
Ivan Vukovic <3.14wee@gmail.com>
2018-01-24 15:28:22 +0100

    Export enums from iOS and document JS methods

--- index.js
+++ index.js
@@ -2,5 +2,74 @@
 import { NativeModules } from 'react-native';
 
 const { RNMobileRTC } = NativeModules;
+const { UserTypes } = RNMobileRTC;
+const DEFAULT_MEETING_OPTIONS = {
+  meetingNumber: '',
+  pwd: '',
+  participantId: '',
+  userName: '',
+};
 
-export default RNMobileRTC;
+export default {
+  ...RNMobileRTC,
+
+  /**
+   * Initializes the Zoom SDK by signing it with SDK Auth.
+   * Call this when you app is mounting.
+   * @param {String} sdkKey SDK key created on https://developer.zoom.us/me/#sdk
+   * @param {String} sdkSecret SDK secret associated with SDK key
+   * @param {String} [sdkDomain='zoom.us'] SDK domain, defaults to zoom.us
+   * @returns {Promise} A promise represents a result of SDK Authentication.
+   * Catch and handle error types from AuthError object
+   */
+  initialize(sdkKey, sdkSecret, sdkDomain = 'zoom.us') {
+    return RNMobileRTC.initialize(sdkKey, sdkSecret, sdkDomain);
+  },
+
+  /**
+   * Starts the new meeting
+   * @param {Object} options Meeting options
+   * @param {String} options.meetingNumber Meeting number
+   * @param {String} options.userName Users' name to be displayed at a meeting
+   * @param {String} [options.pwd] Meeting password
+   * @param {String} [options.participantId] Participant ID
+   * @param {String} [optons.userType=UserTypes.ZOOM_USER] API_USER, ZOOM_USER or SSO_USER user type
+   * from UserTypes object
+   * @param {String} [options.userId] User ID from Zoom REST API, send it only
+   * when using API_USER UserType
+   * @param {String} [options.userToken] User Token from Zoom REST API, send it only
+   * when using API_USER UserType
+   * @returns {Promise} A promise that represents a result of starting the meeting
+   * if resulted with error. Catch and handle error types from MeetingError object
+   */
+  startMeeting(options) {
+    const meetingOptions = {
+      ...DEFAULT_MEETING_OPTIONS,
+      userType: UserTypes.ZOOM_USER,
+      userId: '',
+      userToken: '',
+      ...options,
+    };
+
+    return RNMobileRTC.startMeeting(meetingOptions);
+  },
+
+  /**
+   * Join existing meeting with meeting options
+   * @param {Object} options Meeting options
+   * @param {String} options.meetingNumber Meeting number
+   * @param {String} options.userName Users' name to be displayed at a meeting
+   * @param {String} [options.pwd] Meeting password
+   * @param {String} [options.participantId] Participant ID
+   * @returns {Promise} A promise that represents a result of joining the meeting
+   * if resulted with error. Catch and handle error types from MeetingError object
+   */
+  joinMeeting(options) {
+    const meetingOptions = {
+      ...DEFAULT_MEETING_OPTIONS,
+      ...options,
+    };
+
+    return RNMobileRTC.joinMeeting(meetingOptions);
+  },
+};
--- ios/RNMobileRTC.m
+++ ios/RNMobileRTC.m
@@ -1,7 +1,6 @@
 
 #import "RNMobileRTC.h"
 
-// TODO (Ivan): export user type consants
 // TODO (Ivan): export error codes
 
 NSString * const AuthError_toString[] = {
@@ -76,16 +75,72 @@ RCT_EXPORT_METHOD(joinMeeting:(NSDictionary *) options resolver:(RCTPromiseResol
         ms.delegate = self;
         //For Join a meeting with password
         NSDictionary *paramDict = @{
-                                    kMeetingParam_Username:options[@"userName"],
-                                    kMeetingParam_MeetingNumber:options[@"meetingNumber"],
-                                    kMeetingParam_MeetingPassword:options[@"pwd"],
-                                    kMeetingParam_ParticipantID:options[@"participantId"],
+                                    kMeetingParam_Username: options[@"userName"],
+                                    kMeetingParam_MeetingNumber: options[@"meetingNumber"],
+                                    kMeetingParam_MeetingPassword: options[@"pwd"],
+                                    kMeetingParam_ParticipantID: options[@"participantId"],
                                     };
         MobileRTCMeetError ret = [ms joinMeetingWithDictionary:paramDict];
         NSLog(@"onJoinaMeeting ret:%d", ret);
     }
     _meetingResolver = resolve;
     _meetingRejecter = reject;
+}
+
+#pragma mark - Module constants
+
+- (NSDictionary *)constantsToExport
+{
+    return @{
+             @"UserTypes": @{
+                     @"API_USER": @(MobileRTCUserType_APIUser),
+                     @"ZOOM_USER": @(MobileRTCUserType_ZoomUser),
+                     @"SSO_USER": @(MobileRTCUserType_SSOUser),
+                     },
+             @"MeetingError": @{
+                     //Incorrect meeting number
+                     @"INCORRECT_MEETING_NUMBER": @(MobileRTCMeetError_IncorrectMeetingNumber),
+                     //Meeting Timeout
+                     @"MEETING_TIMEOUT": @(MobileRTCMeetError_MeetingTimeout),
+                     //Network Unavailable
+                     @"NETWORK_UNAVAILABLE": @(MobileRTCMeetError_NetworkUnavailable),
+                     //Client Version Incompatible
+                     @"CLIENT_VERSION_INCOMPATIBLE": @(MobileRTCMeetError_MeetingClientIncompatible),
+                     //User is Full
+                     @"USER_FULL": @(MobileRTCMeetError_UserFull),
+                     //Meeting is over
+                     @"MEETING_OVER": @(MobileRTCMeetError_MeetingOver),
+                     //Meeting does not exist
+                     @"MEETING_NOT_EXIST": @(MobileRTCMeetError_MeetingNotExist),
+                     //Meeting has been locked
+                     @"MEETING_LOCKED": @(MobileRTCMeetError_MeetingLocked),
+                     //Meeting Restricted
+                     @"MEETING_RESTRICTED": @(MobileRTCMeetError_MeetingRestricted),
+                     //JBH Meeting Restricted
+                     @"MEETING_JBH_RESTRICTED": @(MobileRTCMeetError_MeetingJBHRestricted),
+                     
+                     //Invalid Arguments
+                     @"INVALID_ARGUMENTS": @(MobileRTCMeetError_InvalidArguments),
+                     //Invalid Arguments
+                     @"INVALID_USER_TYPE": @(MobileRTCMeetError_InvalidUserType),
+                     //Already In another ongoing meeting
+                     @"IN_ANOTHER_MEETING": @(MobileRTCMeetError_InAnotherMeeting),
+                     //Unknown error
+                     @"UNKNOWN": @(MobileRTCMeetError_Unknown),
+                     },
+             @"AuthError": @{
+                     //Key or Secret is empty
+                     @"KEY_OR_SECRET_EMPTY": @(MobileRTCAuthError_KeyOrSecretEmpty),
+                     //Key or Secret is wrong
+                     @"KEY_OR_SECRET_WRONG": @(MobileRTCAuthError_KeyOrSecretWrong),
+                     //Client Account does not support
+                     @"ACCOUNT_NOT_SUPPORT": @(MobileRTCAuthError_AccountNotSupport),
+                     //Client account does not enable SDK
+                     @"ACCOUNT_NOT_ENABLE_SDK": @(MobileRTCAuthError_AccountNotEnableSDK),
+                     //Auth Unknown error
+                     @"UNKNOWN": @(MobileRTCAuthError_Unknown),
+                     }
+             };
 }
 
 #pragma mark - Auth Delegate
@@ -114,11 +169,11 @@ RCT_EXPORT_METHOD(joinMeeting:(NSDictionary *) options resolver:(RCTPromiseResol
     if (returnValue != MobileRTCAuthError_Success)
     {
         NSString *errorMsg = AuthError_toString[returnValue];
-        _initRejecter(errorMsg, errorMsg, nil);
+        _initRejecter([@(returnValue) stringValue], errorMsg, nil);
     }
     else
     {
-        _initResolver(@"Success!");
+        _initResolver(nil);
     }
     
     _initResolver = nil;
@@ -143,11 +198,11 @@ RCT_EXPORT_METHOD(joinMeeting:(NSDictionary *) options resolver:(RCTPromiseResol
     NSLog(@"onMeetingReturn:%d, internalError:%zd", error, internalError);
     if (error != MobileRTCMeetError_Success)
     {
-        _meetingRejecter(@"error", @"error", nil);
+        _meetingRejecter([@(error) stringValue], @"error", nil);
     }
     else
     {
-        _meetingResolver(@"Meeting!");
+        _meetingResolver(nil);
     }
     
     _meetingResolver = nil;
@@ -158,6 +213,9 @@ RCT_EXPORT_METHOD(joinMeeting:(NSDictionary *) options resolver:(RCTPromiseResol
 - (void)onMeetingError:(NSInteger)error message:(NSString*)message
 {
     NSLog(@"onMeetingError:%zd, message:%@", error, message);
+    _meetingRejecter([@(error) stringValue], message, nil);
+    _meetingRejecter = nil;
+    _meetingResolver = nil;
 }
 
 - (void)onMeetingStateChange:(MobileRTCMeetingState)state

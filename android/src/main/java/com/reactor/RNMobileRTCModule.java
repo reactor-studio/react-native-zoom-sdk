64a15849c3dca763033147c3862519df2676adb0
Ivan Vukovic <3.14wee@gmail.com>
2018-02-01 16:39:24 +0100

    Export constants from Android

--- android/src/main/java/com/reactor/RNMobileRTCModule.java
+++ android/src/main/java/com/reactor/RNMobileRTCModule.java
@@ -15,6 +15,8 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
 import us.zoom.sdk.JoinMeetingOptions;
 import us.zoom.sdk.StartMeetingOptions;
@@ -27,6 +29,10 @@ import us.zoom.sdk.MeetingStatus;
 import us.zoom.sdk.ZoomError;
 import us.zoom.sdk.ZoomSDK;
 import us.zoom.sdk.ZoomSDKInitializeListener;
+
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class RNMobileRTCModule extends ReactContextBaseJavaModule implements MeetingServiceListener, ZoomSDKInitializeListener {
 
@@ -159,65 +165,68 @@ public class RNMobileRTCModule extends ReactContextBaseJavaModule implements Mee
 
 	@Override
   public @Nullable Map getConstants() {
+		MapBuilder.Builder meetingErrorBuilder = MapBuilder.builder();
+
+		///network issue, please check network connection
+		meetingErrorBuilder.put("NETWORK_ERROR", MeetingError.MEETING_ERROR_NETWORK_ERROR);
+		///mmr issue, please check mmr configuration
+		meetingErrorBuilder.put("MMR_ERROR", MeetingError.MEETING_ERROR_MMR_ERROR);
+		///failed to create video and audio data connection with mmr
+		meetingErrorBuilder.put("SESSION_ERROR", MeetingError.MEETING_ERROR_SESSION_ERROR);
+		///the meeting is over
+		meetingErrorBuilder.put("MEETING_OVER", MeetingError.MEETING_ERROR_MEETING_OVER);
+		///the meeting does not exist
+		meetingErrorBuilder.put("MEETING_NOT_EXIST", MeetingError.MEETING_ERROR_MEETING_NOT_EXIST);
+		///the meeting has reached a maximum of participants
+		meetingErrorBuilder.put("USER_FULL", MeetingError.MEETING_ERROR_USER_FULL);
+		///the mobilertc version is incompatible
+		meetingErrorBuilder.put("CLIENT_VERSION_INCOMPATIBLE", MeetingError.MEETING_ERROR_CLIENT_INCOMPATIBLE);
+		///the meeting was locked by host
+		meetingErrorBuilder.put("MEETING_LOCKED", MeetingError.MEETING_ERROR_LOCKED);
+		//Meeting Restricted
+		meetingErrorBuilder.put("MEETING_RESTRICTED", MeetingError.MEETING_ERROR_RESTRICTED);
+		///there does not exist valid mmr
+		meetingErrorBuilder.put("NO_MMR", MeetingError.MEETING_ERROR_NO_MMR);
+		///the meeting was restricted join before host
+		meetingErrorBuilder.put("RESTRICTED_JBH", MeetingError.MEETING_ERROR_RESTRICTED_JBH);
+		///failed to send create meeting command to web server
+		meetingErrorBuilder.put("CANNOT_EMIT_WEB_REQUEST", MeetingError.MEETING_ERROR_WEB_SERVICE_FAILED);
+		///failed to start meeting with expired token
+		meetingErrorBuilder.put("CANNOT_START_TOKEN_EXPIRE", MeetingError.MEETING_ERROR_WEBINAR_ENFORCE_LOGIN);
+		///webinar has reached its maximum
+		meetingErrorBuilder.put("WEBINAR_FULL", MeetingError.MEETING_ERROR_REGISTER_WEBINAR_FULL);
+		///sign in to start the webinar
+		meetingErrorBuilder.put("WEBINAR_HOST_REGISTER", MeetingError.MEETING_ERROR_DISALLOW_HOST_RESGISTER_WEBINAR);
+		///join the webinar from the link
+		meetingErrorBuilder.put("WEBINAR_PANELIST_REGISTER", MeetingError.MEETING_ERROR_DISALLOW_PANELIST_REGISTER_WEBINAR);
+		///host has denied your webinar registration
+		meetingErrorBuilder.put("WEBINAR_DENIED_EMAIL", MeetingError.MEETING_ERROR_HOST_DENY_EMAIL_REGISTER_WEBINAR);
+		///sign in with the specified account to join webinar
+		meetingErrorBuilder.put("WEBINAR_ENFORCE_LOGIN", MeetingError.MEETING_ERROR_WEBINAR_ENFORCE_LOGIN);
+		//Invalid Arguments
+		meetingErrorBuilder.put("INVALID_ARGUMENTS", MeetingError.MEETING_ERROR_INVALID_ARGUMENTS);
+		//Unknown error
+		meetingErrorBuilder.put("UNKNOWN", MeetingError.MEETING_ERROR_UNKNOWN);
+
+
     return MapBuilder.of(
-			"MeetingError", MapBuilder.of(
-				///network issue, please check network connection
-				"NETWORK_ERROR": MeetingError.MEETING_ERROR_NETWORK_ERROR,
-				///mmr issue, please check mmr configuration
-				"MMR_ERROR": MeetingError.MEETING_ERROR_MMR_ERROR,
-				///failed to create video and audio data connection with mmr
-				"SESSION_ERROR": MeetingError.MEETING_ERROR_SESSION_ERROR,
-				///the meeting is over
-				"MEETING_OVER": MeetingError.MEETING_ERROR_MEETING_OVER,
-				///the meeting does not exist
-				"MEETING_NOT_EXIST": MeetingError.MEETING_ERROR_MEETING_NOT_EXIST,
-				///the meeting has reached a maximum of participants
-				"USER_FULL": MeetingError.MEETING_ERROR_USER_FULL,
-				///the mobilertc version is incompatible
-				"CLIENT_VERSION_INCOMPATIBLE": MeetingError.MEETING_ERROR_CLIENT_INCOMPATIBLE,
-				///the meeting was locked by host
-				"MEETING_LOCKED": MeetingError.MEETING_ERROR_LOCKED,
-				//Meeting Restricted
-				"MEETING_RESTRICTED": MeetingError.MEETING_ERROR_RESTRICTED,
-				///there does not exist valid mmr
-				"NO_MMR": MeetingError.MEETING_ERROR_NO_MMR,
-				///the meeting was restricted join before host
-				"RESTRICTED_JBH": MeetingError.MEETING_ERROR_RESTRICTED_JBH,
-				///failed to send create meeting command to web server
-				"CANNOT_EMIT_WEB_REQUEST": MeetingError.MEETING_ERROR_WEB_SERVICE_FAILED,
-				///failed to start meeting with expired token
-				"CANNOT_START_TOKEN_EXPIRE": MeetingError.MEETING_ERROR_WEBINAR_ENFORCE_LOGIN,
-				///webinar has reached its maximum
-				"WEBINAR_FULL": MeetingError.MEETING_ERROR_REGISTER_WEBINAR_FULL,
-				///sign in to start the webinar
-				"WEBINAR_HOST_REGISTER": MeetingError.MEETING_ERROR_DISALLOW_HOST_RESGISTER_WEBINAR,
-				///join the webinar from the link
-				"WEBINAR_PANELIST_REGISTER": MeetingError.MEETING_ERROR_DISALLOW_PANELIST_REGISTER_WEBINAR,
-				///host has denied your webinar registration
-				"WEBINAR_DENIED_EMAIL": MeetingError.MEETING_ERROR_HOST_DENY_EMAIL_REGISTER_WEBINAR,
-				///sign in with the specified account to join webinar
-				"WEBINAR_ENFORCE_LOGIN": MeetingError.MEETING_ERROR_WEBINAR_ENFORCE_LOGIN,
-				//Invalid Arguments
-				"INVALID_ARGUMENTS": MeetingError.MEETING_ERROR_INVALID_ARGUMENTS,
-				//Unknown error
-				"UNKNOWN": MeetingError.MEETING_ERROR_UNKNOWN
-    	),
+			"MeetingError", meetingErrorBuilder.build(),
 			"AuthError", MapBuilder.of(
 				//Key or Secret is empty
-        "KEY_OR_SECRET_EMPTY": ZoomError.ZOOM_ERROR_ILLEGAL_APP_KEY_OR_SECRET,
+        "KEY_OR_SECRET_EMPTY", ZoomError.ZOOM_ERROR_ILLEGAL_APP_KEY_OR_SECRET,
         //Key or Secret is wrong
-        "KEY_OR_SECRET_WRONG": ZoomError.ZOOM_ERROR_ILLEGAL_APP_KEY_OR_SECRET,
+        "KEY_OR_SECRET_WRONG", ZoomError.ZOOM_ERROR_ILLEGAL_APP_KEY_OR_SECRET,
         //Client Account does not support
-        "ACCOUNT_NOT_SUPPORT": ZoomError.ZOOM_ERROR_DEVICE_NOT_SUPPORTED,
+        "ACCOUNT_NOT_SUPPORT", ZoomError.ZOOM_ERROR_DEVICE_NOT_SUPPORTED,
         //Client account does not enable SDK
-        "ACCOUNT_NOT_ENABLE_SDK": ZoomError.ZOOM_ERROR_DEVICE_NOT_SUPPORTED,
+        "ACCOUNT_NOT_ENABLE_SDK", ZoomError.ZOOM_ERROR_DEVICE_NOT_SUPPORTED,
         //Auth Unknown error
-        "UNKNOWN": ZoomError.ZOOM_ERROR_UNKNOWN
+        "UNKNOWN", ZoomError.ZOOM_ERROR_UNKNOWN
 			),
 			"UserType", MapBuilder.of(
-				"API_USER": MeetingService.USER_TYPE_API_USER,
-				"ZOOM_USER": MeetingService.USER_TYPE_ZOOM_USER,
-				"SSO_USER": MeetingService.USER_TYPE_SSO_USER
+				"API_USER", MeetingService.USER_TYPE_API_USER,
+				"ZOOM_USER", MeetingService.USER_TYPE_ZOOM,
+				"SSO_USER", MeetingService.USER_TYPE_SSO
 			)
 		);
   }
@@ -235,11 +244,12 @@ public class RNMobileRTCModule extends ReactContextBaseJavaModule implements Mee
 
   @Override
 	public void onMeetingEvent(int meetingEvent, int errorCode, int internalErrorCode) {		
-		if(meetingEvent == MeetingEvent.MEETING_CONNECT_FAILED && errorCode !== MeetingError.MEETING_ERROR_SUCCESS) {
+		if (meetingEvent == MeetingEvent.MEETING_CONNECT_FAILED &&
+			errorCode != MeetingError.MEETING_ERROR_SUCCESS) {
 			mPromise.reject("" + errorCode);
 		}
 		
-		if(mbPendingStartMeeting && meetingEvent == MeetingEvent.MEETING_DISCONNECTED) {
+		if (mbPendingStartMeeting && meetingEvent == MeetingEvent.MEETING_DISCONNECTED) {
 			mbPendingStartMeeting = false;
 			mPromise.reject("" + errorCode);
 		}
--- scripts/postlink.js
+++ scripts/postlink.js
@@ -2,8 +2,10 @@
 
 const xcode = require('xcode');
 const fs = require('fs');
+const path = require('path');
 const packageJson = require('../../../package.json');
 
+// Postlink iOS
 const frameworks = [
   'libsqlite3.tbd',
   'libstdc++.6.0.9.tbd',
@@ -29,3 +31,18 @@ fs.writeFileSync(
   projectPath,
   pbxProject.writeSync()
 );
+
+// Postlink Android
+const settingsGradlePath = './android/settings.gradle';
+const settingsGradle = fs.readFileSync(settingsGradlePath, 'utf8');
+const getIncludeForProject = (name) => (
+  `include ':${name}'\n` +
+  `project(':${name}').projectDir = ` +
+  `new File(rootProject.projectDir, '${path.join(projectDir, 'android', name)}')\n`
+);
+
+const projectsToInclude = [
+  'zoomcommonlib',
+  'zoomsdk'
+];
+projectsToInclude.map(project => settingsGradle.replace(/\n/, getIncludeForProject(project)));
